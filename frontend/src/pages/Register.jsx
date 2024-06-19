43b3a6bc421b051822a67c01bee0327f8bdf5996
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-06-19 21:13:45 +0530

    clear the small bug

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,7 +5,7 @@ import { toastError, toastInfo, toastSuccess } from '../utils/Toast';
 function Register() {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
-  const [validationmessage, setValidationMessage] = useState({});
+  // const [validationmessage, setValidationMessage] = useState({});
 
   const navigate = useNavigate();
 
@@ -14,72 +14,10 @@ function Register() {
       ...formData,
       [e.target.id]: e.target.value
     })
-  }
-
-  const validationMessage = (e) => {
-    const { name, email, phonenumber } = formData;
-
-    let message = '';
-    if (name === '')  {
-      message = 'Name is required';
-    } else if (value.length < 2) {
-      message = 'Name must be at least 2 characters';
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const phoneRegex = /^\d{10}$/;
-
-    if ( email === '') {
-      message = 'Email is required';
-    } else if (!emailRegex.test(value)) {
-      message = 'Invalid email address'
-    }
-
-    if (phonenumber === '') {
-      message = 'Phonenumber is required';
-    } else if (!phoneRegex.test(value)) {
-      message = 'Phone number must be 10 digits long';
-    }
-    return message;
-    setValidationMessage({
-      ...validationMessage,
-      [name] : message
-    })
-  }
-
-  const validateInput = (e) => {
-    const name = formData;
-    let message = '';
-    if (name === 'name') {
-      if(!value) {
-        message = 'Name is required';
-      } else if (value.length < 2) {
-        message = 'Name must be at least 2 characters';
-      } else {
-        message = '';
-      }
-    } else if (name === 'email') {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(value)) {
-        message = 'Invalid email address.';
-      } else {
-        message = 'Valid email.';
-      }
-    } else if (name === 'phoneNumber') {
-      const phoneRegex = /^\d{10}$/;
-      if (!phoneRegex.test(value)) {
-        message = 'Phone number must be 10 digits long.';
-      } else {
-        message = 'Valid phone number.';
-      }
-    }
-    return message;
-  }
+  }  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    validationMessage(e);
-  
     try {
       setLoading(true);
       const res = await fetch('/api/users/create' ,{
