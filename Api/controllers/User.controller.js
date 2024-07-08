ac2bcb4d3cd4239f327740eb98985a664737862a
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-07-08 13:27:18 +0530

    modified in user controllers

--- Api/controllers/User.controller.js
+++ Api/controllers/User.controller.js
@@ -1,12 +1,14 @@
-import User from '../models/User.model.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { sendEmail } from './Email.controller.js';
+import User from '../models/User.model.js';
 
 export const userRegister = async(req, res) => {
-  const { name, email, phonenumber} = req.body;
+  const { name, email, password } = req.body;
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-  const phoneRegex = /^[0-9]{10}$/;
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
-  if (!name || !email || !phonenumber) {
+  if (!name || !email || !password) {
     return res.status(400).json({
       message: "All fields are required",
       success: false,
@@ -18,13 +20,7 @@ export const userRegister = async(req, res) => {
       success: false,
       status: 400
     });
-  } else if (!phoneRegex.test(phonenumber)) {
-    return res.status(400).json({
-      message: "Invalid phonenumber",
-      success: false,
-      status: 400
-    });
-  }
+  } 
 
   if (email) {
     const user = await User.findOne({ email });
@@ -40,7 +36,7 @@ export const userRegister = async(req, res) => {
   const newUser = new User({
     name,
     email,
-    phonenumber,
+    password: hashedPassword,
   });
 
   try {
@@ -54,4 +50,73 @@ export const userRegister = async(req, res) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+export const userLogin = async(req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+      success: false,
+      status: 404
+    });
+  }
+
+  const isMatch = bcrypt.compareSync(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({
+      message: "Invalid credentials",
+      success: false,
+      status: 401
+    });
+  }
+
+  // Generate JWT token
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  const expiryDate = new Date(Date.now() + 3600000);
+  res
+      .cookie('access_token', token, {httpOnly: true, expires: expiryDate})
+      .status(200)
+      .json({
+        message: "Logged in successfully",
+        success: true,
+        status: 200,
+        token,
+        expiryDate
+      })
+
+  // Remember to add middleware to validate JWT token before accessing protected routes
+  // add middleware to validate JWT token before accessing protected routes
+  // middleware: (req, res, next) => {
+  //   const token = req.header('Authorization').split(' ')[1];
+  //   if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
+
+  //   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  //     if (err) return res.status(403).json({ message: 'Access denied. Invalid token.' });
+  //     req.user = user;
+  //     next();
+  //   });
+  // 
+};
+
+export const userLogout = async(req, res) => {
+  const { id } = req.params;
+  const user = await User.findByIdAndUpdate(id, { isOnline: false });
+
+  if (!user) return res.json({ message: 'Invalid token.' });
+
+  res.clearCookie('access_token').status(200).json({ message: 'Logged out successfully.' });
+  
+
+  // Remember to add middleware to validate JWT token before accessing protected routes
+  // middleware: (req, res, next) => {
+  //   const token = req.header('Authorization').split(' ')[1];
+  //   if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
+  //   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  //     if (err) return res.status(403).json({ message: 'Access denied. Invalid token.' });
+  //     req.user = user;
+  //     next();
+  //   });
+  // }
 };
--- Api/routes/User.route.js
+++ Api/routes/User.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { userRegister } from '../controllers/User.controller.js';
+import { userLogin, userLogout, userRegister } from '../controllers/User.controller.js';
 
 const router = express.Router();
 
-router.post('/create', userRegister);
+router.post('/register', userRegister);
+router.post('/login', userLogin);
+router.post('/logout', userLogout);
 
 export default router;
