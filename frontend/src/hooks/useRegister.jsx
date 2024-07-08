30d6353b3d08e855ee430c6aecfaa7cc141b40a7
Dineshkumar <117102544+unknown001dk@users.noreply.github.com>
2024-07-08 09:14:27 +0530

    create own hooks for Register

---
+++ frontend/src/hooks/useRegister.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { toastError, toastSuccess } from '../utils/Toast';
+
+export default function useRegister() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [loader, SetLoader] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      SetLoader(false);
+    },3000);
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/api/users/create' ,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      const message = data.message;
+      const success = data.success;
+
+      if(success === true) {
+        toastSuccess(message);
+        navigate('/success-form');
+      } else {
+        toastError(message);
+      }
+      setLoading(false);  
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      toastError(res.message);
+    }
+  } 
+
+  return { formData, handleChange, handleSubmit, loading, loader }
+}
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,57 +1,11 @@
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { toastError, toastSuccess } from '../utils/Toast';
+import useRegister from '../hooks/useRegister';
 import BounceLoader from 'react-spinners/BounceLoader'
 import { PageTitle } from '../components/pageTitle';
 
 function Register() {
-  const [formData, setFormData] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [loader, SetLoader] = useState(true);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    setTimeout(() => {
-      SetLoader(false);
-    },3000);
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value
-    })
-  }  
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const res = await fetch('/api/users/create' ,{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-      const data = await res.json();
-      const message = data.message;
-      const success = data.success;
-
-      if(success === true) {
-        toastSuccess(message);
-        navigate('/success-form');
-      } else {
-        toastError(message);
-      }
-      setLoading(false);  
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-      toastError(res.message);
-    }
-  } 
+  const {
+    handleChange, handleSubmit, loading, loader
+  } = useRegister();
   
   return (
     <div>
@@ -86,7 +40,6 @@ function Register() {
                 className='bg-slate-100 p-3 rounded-lg'
                 placeholder='Phonenumber'
                 id='phonenumber'
-                // pattern="[0-9]{5}[0-9]{5}"
                 />
 
                 <button
